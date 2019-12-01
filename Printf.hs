768042e7be4ed9172eac8a7c3e40b0bfd7a7ad13
ivanbrennan <ivan.brennan@gmail.com>
2019-12-01 18:31:10 -0500

    add string formatting

--- Printf.hs
+++ Printf.hs
@@ -4,14 +4,16 @@ module Printf where
 
 import Language.Haskell.TH (Q, Exp, stringE)
 
-data Format = D | Literal String
+data Format = D | S | Literal String
 
 parse :: String -> Format
 parse "%d" = D
+parse "%s" = S
 parse s    = Literal s
 
 gen :: Format -> Q Exp
 gen D           = [| \n -> show n |]
+gen S           = [| \s -> s |]
 gen (Literal s) = stringE s
 
 pr :: String -> Q Exp
