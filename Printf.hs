d3e4fb52df411737f1696fe42759c5aeb67d6465
ivanbrennan <ivan.brennan@gmail.com>
2019-12-01 18:04:48 -0500

    data Format
    Introduce the Format type that will express different varieties of
    expressions we may parse.

--- Printf.hs
+++ Printf.hs
@@ -4,5 +4,13 @@ module Printf where
 
 import Language.Haskell.TH (Q, Exp, stringE)
 
+data Format = Literal String
+
+parse :: String -> Format
+parse s = Literal s
+
+gen :: Format -> Q Exp
+gen (Literal s) = stringE s
+
 pr :: String -> Q Exp
-pr s = stringE s
+pr s = gen (parse s)
